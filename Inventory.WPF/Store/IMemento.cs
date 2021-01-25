c154b7c01bdf7c4bf27e4d8a23d2ad5462f75309
magdalenaryznar <66912315+magdalenaryznar@users.noreply.github.com>
2021-01-25 19:58:39 +0100

    Inventory App setup MVVM

---
+++ Inventory.WPF/Store/IMemento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.WPF.Mementors
+{
+    public interface IMemento
+    {
+        void Restore();
+    }
+}
